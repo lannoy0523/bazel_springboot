127a2320612591ac928bd1b83418ff112d1e3487
v_wuqdwu <v_wuqdwu@tencent.com>
2023-04-11 17:49:23 +0800

    Initial commit

---
+++ src/main/java/com/example/maven_springboot/MavenSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenSpringbootApplication.class, args);
+    }
+
+}
